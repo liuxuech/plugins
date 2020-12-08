40fbb891589167b20c6da63468c992830741d5d8
liuxuech <yiwaicanxue@163.com>
2020-12-08 23:16:52 +0800

    update

---
+++ go.mod
@@ -0,0 +1,5 @@
+module plugins
+
+go 1.13
+
+require github.com/liuxuech/packages v0.0.1 // indirect
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/liuxuech/packages/store"
+)
+
+func main() {
+
+}
